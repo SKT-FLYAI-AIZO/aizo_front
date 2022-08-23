6d796b029026f0be7b94ec1ede9a2ad6ce482155
sungkyum <sungkyum1@naver.com>
2022-08-23 16:52:11 +0900

    add SettingScreen

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
-        SettingScreen
-      </Text>
+      <View style={styles.iconview}>
+        <TouchableOpacity style={styles.button}>
+          <Text>알림버튼</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.title}>
+        <Text style={ {fontSize:35,}} >설정</Text>
+      </View>
+      <View style={styles.settingBox}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.account}>
+            계정
+          </Text> 
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.notice}>
+            알림
+          </Text> 
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.withdrawal}>
+            회원탈퇴
+          </Text> 
+        </TouchableOpacity>
+
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -14,8 +40,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  iconview:{
+    flex:1,
+    backgroundColor:'#fff',
+    justifyContent: "center",
+    alignItems:'flex-end',
+    width:'100%',
+  },
+  title:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width:'100%'
+  },
+  settingBox:{
+    flex:6,
+    backgroundColor:'#fff',
+    width:'75%',
+    margin:30
+  },
+  account:{
+    fontSize:35,
+    color:'#333',
+  },
+  notice:{
+    fontSize:35,
+    color:'#333',
+  },
+  withdrawal:{
+    fontSize:35,
+    color:'#333',
+  },
+  btn:{
+    borderBottomWidth:2,
+    borderColor:'gray'
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
   },
 });
